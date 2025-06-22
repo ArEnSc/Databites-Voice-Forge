67ed63a360bc8af865a371c583e7c28a4e811afa
CrossPr0duct <alicextensor@gmail.com>
2025-06-21 23:12:37 -0400

    redeploying again.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Databites-Voice-Forge/', // Replace 'project' with your repository name
+  base: './', // Replace 'project' with your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
